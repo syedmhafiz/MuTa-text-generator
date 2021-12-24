9cbdf2509c005a291096e0c231c6a7b44e773e9b
syedomee <syedhafiz61@gmail.com>
2021-12-24 19:27:48 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import random as rd
+
+def raDU(ch):  
+    ch = ch.upper()
+
+    return ch
+
+def raDL(ch):    
+    ch = ch.lower()
+
+    return ch
+
+def gene(txt):
+    new_txt = ""
+    for i in range(len(txt)):
+        re = rd.randint(2, 5) 
+
+        if ord(txt[i]) in range(97, 123):
+           if re % 2:
+               new_txt += raDL(txt[i])
+           else:
+                new_txt += raDU(txt[i])
+        elif ord(txt[i]) in range(65, 91):
+            if re % 2:
+               new_txt += raDL(txt[i])
+            else:
+                new_txt += raDU(txt[i])               
+        else:
+            new_txt += txt[i]    
+        
+    return new_txt        
+
+
+if __name__ == "__main__":
+    q = input("Enter the text(5 - 30 characters max):")
+    w = gene(q)
+    print(w)
