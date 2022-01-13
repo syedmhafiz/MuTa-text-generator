66c4dbecc4622016e4007f98212e9c39aca36d81
syedomee <syedhafiz61@gmail.com>
2022-01-13 13:02:12 +0600

    Intensity added

--- main.py
+++ main.py
@@ -10,10 +10,18 @@ def raDL(ch):
 
     return ch
 
+def intensify(an):
+    if an == 1:
+        return rd.randint(2, 10)
+    elif an == 2:
+        return rd.randint(2, 5)
+    else:
+        return rd.randint(2, 3)        
+
 def gene(txt):
     new_txt = ""
     for i in range(len(txt)):
-        re = rd.randint(2, 5) 
+        re = intensify(intensity)
 
         if ord(txt[i]) in range(97, 123):
            if re % 2:
@@ -33,5 +41,6 @@ def gene(txt):
 
 if __name__ == "__main__":
     q = input("Enter the text(5 - 30 characters max):")
+    intensity = int(input("Enter the intensity level out of 1 to 3: "))
     w = gene(q)
     print("The murad takla version of your text is: ", w)
